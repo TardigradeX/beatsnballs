4ad6cfe31d3783227b49c83e6e4f21e768858c50
daniel.xander <daniel.xander@fhnw.ch>
2016-08-11 23:42:05 +0200

    began with forms

--- bnb/src/app/registration/registration.component.ts
+++ bnb/src/app/registration/registration.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, REACTIVE_FORM_DIRECTIVES  } from '@angular/forms';
+import {AsyncStates} from '../shared/async.states';
+import { Http, Headers } from '@angular/http';
 
 @Component({
   moduleId: module.id,
   selector: 'app-registration',
   templateUrl: 'registration.component.html',
-  styleUrls: ['registration.component.css']
+  styleUrls: ['registration.component.css'],
+  directives: [REACTIVE_FORM_DIRECTIVES]
 })
+
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  public asyncStates = AsyncStates; // Have to declare it so that template can see it.
+  private currentState = AsyncStates.NONE;
+  private form: FormGroup;
+
+  constructor(fb: FormBuilder, public http: Http) {
+    this.form = fb.group({
+      'email': ['', Validators.compose([Validators.required, CustomValidators.lowerCaseLetters, Validators.minLength(4)])],
+      'team_name': ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      'playerOne': ['', Validators.required],
+      'playerTwo': ['', Validators.required],
+      'playerThree': ['', Validators.compose([Validators.required, CustomValidators.mailFormat])],
+    });
+  }
 
   ngOnInit() {
   }
--- bnb/src/app/shared/async.states.ts
+++ bnb/src/app/shared/async.states.ts
@@ -1,4 +1,5 @@
 export enum AsyncStates {
+  NONE,
   ASYNC_PENDING,
   ASYNC_SUCCESSFUL,
   ASYNC_ERROR
